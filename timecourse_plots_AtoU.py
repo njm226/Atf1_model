0a7b3a4ccbf73354002ce39fd6e3d0b2ad4b5c3f
njm226 <62432161+njm226@users.noreply.github.com>
2020-11-18 11:03:49 +0100

    Update timecourse_plots_AtoU.py

--- timecourse_plots_AtoU.py
+++ timecourse_plots_AtoU.py
@@ -19,8 +19,8 @@ pool = multiprocessing.Pool(multiprocessing.cpu_count())
 
 
 
-X_Y_atf1_on=[[182,49,120], 0]
-X_Y_atf1_off=[[182,49,120], 1]
+X_Y_atf1_on=[[182,49,120]]
+X_Y_atf1_off=[[182,49,120]]
 
 
 reps=10000
